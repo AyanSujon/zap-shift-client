397e33499294be1c826a40370b854fe617493223
Ayan <sujon.seo.bd@gmail.com>
2025-11-17 21:25:09 +0600

    added user profile update function

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from '../../firebase/firebase.init';
 
 const googleProvider = new GoogleAuthProvider();
@@ -35,6 +35,9 @@ const AuthProvider = ({ children }) => {
        return signOut(auth);
     }
 
+    const updateUserProfile = (profile)=>{
+        return updateProfile(auth.currentUser, profile)
+    }
 
 
 
@@ -71,6 +74,8 @@ const AuthProvider = ({ children }) => {
         signInUser,
         signinGoogle,
         logOut,
+        updateUserProfile,
+        
 
     }
 
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -3,19 +3,50 @@ import { useForm } from 'react-hook-form';
 import useAuth from '../../../hooks/useAuth';
 import { Link } from 'react-router';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import axios from 'axios';
 
 const Register = () => {
+
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const { registerUser } = useAuth();
+    const { registerUser, updateUserProfile } = useAuth();
 
 
 
 
     const handleRegistration = (data) => {
-        // console.log("After Submit", data);
+        console.log("After Submit", data.photo[0]);
+
+        const profileImg = data.photo[0];
         registerUser(data.email, data.password)
             .then((result) => {
                 console.log(result.user);
+                // 1.  store the image and get the photo url
+                const formData = new FormData();
+                formData.append('image', profileImg);
+                // 2. send the photo to store and get the url
+                const image_API_Url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host_key}`
+                axios.post(image_API_Url, formData)
+                .then(res =>{
+                    console.log('After image upload: ', res.data.data.url);
+                    // 3. update user profile to firebase 
+                    const userProfile = {
+                        displayName: data.name,
+                        photoURL: res.data.data.url
+                    }
+                    // now call firebase function
+                    updateUserProfile(userProfile)
+                    .then(()=> {
+                        console.log('User profile updated done.')
+                    })
+                    .catch(error => console.log(error))
+
+
+
+                })
+
+
+                
+
             })
             .catch((error) => {
                 console.log(error);
@@ -35,11 +66,24 @@ const Register = () => {
             <p className='text-center'>Register with ZapShift</p>
             <form className='card-body' onSubmit={handleSubmit(handleRegistration)}>
                 <fieldset className="fieldset">
+
+
+                    {/* Name */}
+                    <label className="label">Name</label>
+                    <input type="text" {...register('name', { required: true })} className="input w-full" placeholder="Your name" />
+                    {errors.name?.type === 'required' && (<p className='text-red-500'>Name is required.</p>)}
+
+                    {/* Photo */}
+                    <label className="label">Photo</label>
+                    <input type="file" {...register('photo', { required: true })} className="file-input w-full" placeholder="Your name" />
+                    {errors.photo?.type === 'required' && (<p className='text-red-500'>Photo is required.</p>)}
+
+                    {/* Email */}
                     <label className="label">Email</label>
                     <input type="email" {...register('email', { required: true })} className="input w-full" placeholder="Email" />
                     {errors.email?.type === 'required' && (<p className='text-red-500'>Email is required.</p>)}
 
-
+                    {/* Password */}
                     <label className="label">Password</label>
                     <input type="password" {...register('password', { required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/ })} className="input w-full" placeholder="Password" />
                     {errors.password?.type === 'required' && (<p className='text-red-500'>Password is required.</p>)}
@@ -49,7 +93,7 @@ const Register = () => {
                     <button className="btn bg-primary text-black mt-4">Register</button>
                 </fieldset>
                 <p>Already have an account? <Link to={'/login'} className='text-primary link-hover'>Login</Link></p>
-                
+
             </form>
             <SocialLogin></SocialLogin>
         </div>
