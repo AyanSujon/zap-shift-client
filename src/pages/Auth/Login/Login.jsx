51d7798e144f76ba20ac81118834160d036a006e
Ayan <sujon.seo.bd@gmail.com>
2025-11-17 13:36:01 +0600

    added login page functionalities

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,  formState: { errors } } = useForm();
 
     const handleLogin = (data) => {
         console.log("After Submit: ", data);
@@ -17,11 +17,19 @@ const Login = () => {
                 <fieldset className="fieldset">
                     {/* Eamil Field */}
                     <label className="label">Email</label>
-                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+                    <input type="email" {...register('email', {required: true})} className="input" placeholder="Email" />
+                    {
+                        errors.email?.type === 'required' && (<p className='text-red-500'>Eamil is required.</p>)
+                    }
 
                     {/* password Field */}
                     <label className="label">Password</label>
-                    <input type="password" {...register('password')} className="input" placeholder="Password" />
+                    <input type="password" {...register('password', {required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/})} className="input" placeholder="Password" />
+                    {errors.password?.type === 'required' && (<p className='text-red-500'>Password is required.</p>)}
+                    {errors.password?.type === 'minLength' && (<p className='text-red-500'>Password must be 6 characters or longer.</p>)}
+                    {errors.password?.type === 'pattern' && (<p className='text-red-500'>Password must contain at least one uppercase, one lowercase, one number, and one special character.</p>)}
+                    
+                   
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
                 </fieldset>
