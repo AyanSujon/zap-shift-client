8ffb49c79668172b18cab1413363208027e2ea93
Ayan <sujon.seo.bd@gmail.com>
2025-11-17 12:31:56 +0600

    added auth context

---
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/firebase.init';
+
+const AuthProvider = ({children}) => {
+
+
+// new user register
+    const registerUser = (email, password)=> {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // signin user
+    const signInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const authInfo = {
+        registerUser,
+        signInUser,
+        
+
+    }
+
+
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const Register = () => {
-    const { register, handleSubmit, formState: { errors }, } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
 
 
 
@@ -26,9 +26,10 @@ const handleRegistration = (data) =>{
                     
                     
                     <label className="label">Password</label>
-                    <input type="password" {...register('password', {required: true, minLength: 6})} className="input" placeholder="Password" />
+                    <input type="password" {...register('password', {required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=[\]{};':"\\|,.<>\/?`~]).{6,}$/})} className="input" placeholder="Password" />
                     {errors.password?.type === 'required' && (<p className='text-red-500'>Password is required.</p>)}
                     {errors.password?.type === 'minLength' && (<p className='text-red-500'>Password must be 6 characters or longer.</p>)}
+                    {errors.password?.type === 'pattern' && (<p className='text-red-500'>Password must contain at least one uppercase, one lowercase, one number, and one special character.</p>)}
                     
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Register</button>
