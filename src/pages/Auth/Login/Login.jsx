5292fa416ce6df8b8e22fe430dc7b64e6d4bd026
Ayan <sujon.seo.bd@gmail.com>
2025-11-17 13:26:19 +0600

    form functionalities added

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const handleLogin = (data) => {
+        console.log("After Submit: ", data);
+    }
+
+
+
     return (
-        <div>
-            login
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSubmit(handleLogin)} className="card-body">
+                <fieldset className="fieldset">
+                    {/* Eamil Field */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+
+                    {/* password Field */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password')} className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
 
 const Register = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { registerUser} = useAuth();
+
 
 
 
 const handleRegistration = (data) =>{
-    console.log("After Submit", data);
+    // console.log("After Submit", data);
+    registerUser(data.email, data.password)
+    .then((result) => {
+        console.log(result.user);
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
+
 }
 
 
@@ -26,7 +37,7 @@ const handleRegistration = (data) =>{
                     
                     
                     <label className="label">Password</label>
-                    <input type="password" {...register('password', {required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=[\]{};':"\\|,.<>\/?`~]).{6,}$/})} className="input" placeholder="Password" />
+                    <input type="password" {...register('password', {required: true, minLength: 6, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/})} className="input" placeholder="Password" />
                     {errors.password?.type === 'required' && (<p className='text-red-500'>Password is required.</p>)}
                     {errors.password?.type === 'minLength' && (<p className='text-red-500'>Password must be 6 characters or longer.</p>)}
                     {errors.password?.type === 'pattern' && (<p className='text-red-500'>Password must contain at least one uppercase, one lowercase, one number, and one special character.</p>)}
