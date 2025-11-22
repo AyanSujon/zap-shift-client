f509df0d3b9472ae00d276f575a840c96256272e
Ayan <sujon.seo.bd@gmail.com>
2025-11-22 11:14:27 +0600

    delete functionalities added

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,18 +2,67 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { FiEdit } from "react-icons/fi";
+import { FaMagnifyingGlass, FaRegTrashCan } from 'react-icons/fa6';
+import Swal from 'sweetalert2';
+
 
 const MyParcels = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['myParcels', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
         }
     })
+
+    const handleParcelDelete = (id) => {
+        console.log(id);
+
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.deletedCount) {
+                            // Refresh the data in the ui
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your parcel request has been deleted.",
+                                icon: "success"
+                            });
+                        }
+
+                    })
+
+
+
+
+
+
+
+            }
+        });
+
+    }
+
+
+
+
     return (
         <div>
             <h2>All of My Parcels: {parcels.length}</h2>
@@ -36,11 +85,23 @@ const MyParcels = () => {
                         {
                             parcels.map((parcel, index) =>
                                 <tr key={parcel._id}>
-                                    <th>{index +1}</th>
+                                    <th>{index + 1}</th>
                                     <td>{parcel.parcelName}</td>
                                     <td>{parcel.cost}</td>
                                     <td>Blue</td>
-                                    <td>Blue</td>
+                                    <td>
+                                        <button className='btn btn-square hover:bg-primary'>
+                                            <FaMagnifyingGlass />
+                                        </button>
+                                        <button className='btn btn-square hover:bg-primary mx-2'>
+                                            <FiEdit />
+                                        </button>
+                                        <button
+                                            onClick={() => handleParcelDelete(parcel._id)}
+                                            className='btn btn-square hover:bg-primary'>
+                                            <FaRegTrashCan />
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         }
